953259c5c821d71f419a6157b2b3c3fb27f0fca6
Alexey Koleda <alexeykoleda@icloud.com>
2021-12-14 18:15:26 +0300

    Initial Commit

---
+++ CryptocurrencyTracker/CryptocurrencyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptocurrencyTrackerApp.swift
+//  CryptocurrencyTracker
+//
+//  Created by Alexey Koleda on 14.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptocurrencyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
