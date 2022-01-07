97371254ebf260009c7a25205f1066246b9f5a98
Alexey Koleda <alexeykoleda@icloud.com>
2022-01-07 23:58:12 +0300

    Added CoinDetailView

--- CryptocurrencyTracker/Core/Detail/ViewModels/DetailViewModel.swift
+++ CryptocurrencyTracker/Core/Detail/ViewModels/DetailViewModel.swift
@@ -10,20 +10,85 @@ import Combine
 
 class DetailViewModel: ObservableObject {
     
+    @Published var overviewStatistics: [Statistic] = []
+    @Published var additionalStatistics: [Statistic] = []
+    @Published var coin: Coin
+    
     private let coinDetailService: CoinDetailDataService
     private var cancellables = Set<AnyCancellable>()
     
     init(coin: Coin) {
+        self.coin = coin
         self.coinDetailService = CoinDetailDataService(coin: coin)
         self.addSubscribers()
     }
     
     private func addSubscribers() {
+        
+        // Updates CoinDetails
         coinDetailService.$coinDetails
-            .sink { returnedCoinDetails in
-                print(returnedCoinDetails)
+            .combineLatest($coin)
+            .map(mapDataToStatistics)
+            .sink { [weak self] returnedArrays in
+                self?.overviewStatistics = returnedArrays.overview
+                self?.additionalStatistics = returnedArrays.additional
             }
             .store(in: &cancellables)
+    }
+    
+    private func mapDataToStatistics(details: CoinDetail?, coin: Coin) -> (overview: [Statistic], additional: [Statistic]) {
+        let overviewArray = fillOverviewArray(coin: coin)
+        let additionalArray = fillAdditionalArray(coin: coin, details: details)
+        return (overviewArray, additionalArray)
+    }
+    
+    private func fillOverviewArray(coin: Coin) -> [Statistic] {
+        let price = coin.currentPrice.asCurrencyWith6Decimals()
+        let pricePercentageChange = coin.priceChangePercentage24H
+        let priceStat = Statistic(title: "Current Price", value: price, percentageChange: pricePercentageChange)
+        
+        let marketCap = "$" + (coin.marketCap?.formattedWithAbbreviations() ?? "")
+        let marketCapPercentageChange = coin.marketCapChangePercentage24H
+        let marketCapStat = Statistic(title: "Market Capitalisation", value: marketCap, percentageChange: marketCapPercentageChange)
+        
+        let rank = "\(coin.rank)"
+        let rankStat = Statistic(title: "Rank", value: rank)
+        
+        let volume = "$" + (coin.totalVolume?.formattedWithAbbreviations() ?? "")
+        let volumeStat = Statistic(title: "Volume", value: volume)
+
+        let overviewArray: [Statistic] = [
+            priceStat, marketCapStat, rankStat, volumeStat
+        ]
+        return overviewArray
+    }
+    
+    private func fillAdditionalArray(coin: Coin, details: CoinDetail?) -> [Statistic] {
+        let high = coin.high24H?.asCurrencyWith2Decimals() ?? "n/a"
+        let highStat = Statistic(title: "26h High", value: high)
+
+        let low = coin.low24H?.asCurrencyWith2Decimals() ?? "n/a"
+        let lowStat = Statistic(title: "26h Low", value: low)
+        
+        let priceChange = coin.priceChange24H?.asCurrencyWith6Decimals() ?? "n/a"
+        let pricePercentageChange = coin.priceChangePercentage24H
+        let priceChangeStat = Statistic(title: "24h Price Change", value: priceChange, percentageChange: pricePercentageChange)
+        
+        let marketCapChange = "$" + (coin.marketCapChange24H?.formattedWithAbbreviations() ?? "")
+        let marketCapPercentageChange = coin.marketCapChangePercentage24H
+        let marketCapChangeStat = Statistic(title: "24h Market Cap Change", value: marketCapChange, percentageChange: marketCapPercentageChange)
+        
+        let blockTime = details?.blockTimeInMinutes ?? 0
+        let blockTimeString = blockTime == 0 ? "n/a" : "\(blockTime)"
+        let blockTimeStat = Statistic(title: "Block Time", value: blockTimeString)
+        
+        let hashing = details?.hashingAlgorithm ?? "n/a"
+        let hashingStat = Statistic(title: "Hashing Algorithm", value: hashing)
+        
+        let additionalArray: [Statistic] = [
+            highStat, lowStat, priceChangeStat, marketCapChangeStat, blockTimeStat, hashingStat
+        ]
+        return additionalArray
     }
     
 }
